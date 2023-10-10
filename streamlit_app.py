8888393bd092a3b5097bb50211e48efbb24fb302
praveenkrjha <praveenkrjha@gmail.com>
2023-10-10 15:41:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My first streamlit app')
 
-streamlit.header("Main menu")
-streamlit.text("Item1")
-streamlit.text("Item2")
-streamlit.text("Item3")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
+
